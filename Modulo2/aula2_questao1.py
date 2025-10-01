4ce8370b8eff6a068041a13fda644868a59ee071
kefersson20 <keferssonkeke@hotmail.com>
2025-10-01 09:15:50 -0300

    Delete Modulo2/aula2_questao1.py

--- Modulo2/aula2_questao1.py
+++
@@ -1,8 +0,0 @@
-
-num1 = 5
-num2 = 2
-print("Tipo de num1:", type(num1))
-print("Tipo de num2:", type(num2))
-resultado = num1 / num2
-print("Valor do resultado:", resultado)
-print("Tipo do resultado:", type(resultado))
