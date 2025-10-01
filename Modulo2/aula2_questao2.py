b1384ea78e0554522a199f8bd8aab0a5505583c7
kefersson20 <keferssonkeke@hotmail.com>
2025-10-01 09:32:50 -0300

    Add files via upload

---
+++ Modulo2/aula2_questao2.py
@@ -0,0 +1,19 @@
+# aula2_questao2.py
+
+# Armazene o texto "o resultado é" em uma variável
+texto = "o resultado é"
+
+# Armazene o valor 10 em outra variável
+num1 = 10
+
+# Armazene o valor 3.5 em outra variável
+num2 = 3.5
+
+# Some os valores da segunda e terceira variável e armazene em outra variável
+soma = num1 + num2
+
+# Imprima todas as variáveis na ordem de criação e seus tipos
+print(texto, "- Tipo:", type(texto))
+print(num1, "- Tipo:", type(num1))
+print(num2, "- Tipo:", type(num2))
+print(soma, "- Tipo:", type(soma))
