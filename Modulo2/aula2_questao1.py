87c8118068670a60a56b87e1d8f349e65e724d59
kefersson20 <keferssonkeke@hotmail.com>
2025-10-01 09:16:15 -0300

    Add files via upload

---
+++ Modulo2/aula2_questao1.py
@@ -0,0 +1,16 @@
+# aula2_questao1.py
+
+# Armazene o valor 5 em uma variável e o valor 2 em outra variável
+num1 = 5
+num2 = 2
+
+# Imprima o tipo de dados dessas duas variáveis
+print("Tipo de num1:", type(num1))
+print("Tipo de num2:", type(num2))
+
+# Divida a primeira variável pela segunda e armazene o resultado em uma terceira variável
+resultado = num1 / num2
+
+# Imprima a terceira variável e também o seu tipo
+print("Valor do resultado:", resultado)
+print("Tipo do resultado:", type(resultado))
