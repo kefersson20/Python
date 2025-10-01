ad29103d4c5dee2679c2f931124a742f91aa02cd
kefersson20 <keferssonkeke@hotmail.com>
2025-10-01 09:44:37 -0300

    Add files via upload

---
+++ Modulo2/aula2_questao3.py
@@ -0,0 +1,14 @@
+# aula2_questao3.py
+
+# Variáveis iniciais
+v1 = 10
+v2 = 20
+
+# Use uma variável auxiliar para trocar os valores
+aux = v1
+v1 = v2
+v2 = aux
+
+# Imprima os valores após a troca
+print(v1)
+print(v2)
