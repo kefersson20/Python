e58c837ce962b333a020f9b7efcf128b986bbfce
kefersson20 <keferssonkeke@hotmail.com>
2025-10-01 09:58:43 -0300

    Add files via upload

---
+++ Modulo2/aula2_questao4.py
@@ -0,0 +1,16 @@
+# aula2_questao4.py
+
+# Taxa de juros de 1% ao mês
+juros = 1.01
+
+# Saldo inicial
+saldo = 500.0
+
+# Calcule os 3 meses de rendimento usando apenas duas variáveis
+saldo = saldo * juros  # após 1º mês
+saldo = saldo * juros  # após 2º mês
+saldo = saldo * juros  # após 3º mês
+
+# Imprima o resultado
+print("Após 3 meses meu novo saldo é")
+print(saldo)
